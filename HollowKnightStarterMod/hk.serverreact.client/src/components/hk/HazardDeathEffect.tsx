dcb0cc11ec8dcd9fd21f12edd80554eeeec529d3
JaessDev <jaess-dev@gmx.de>
2026-01-25 14:58:29 +0100

    optimize imports

--- HollowKnightStarterMod/hk.serverreact.client/src/components/hk/HazardDeathEffect.tsx
+++ HollowKnightStarterMod/hk.serverreact.client/src/components/hk/HazardDeathEffect.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useDeathEvent, useGrubSavedEvent, useHazardDeathEvent } from "../../lib/signalr/useSignalr";
 import type { EventType } from "./HkEffect";
 import HollowKnightEventOverlay from "./HkEffect";
