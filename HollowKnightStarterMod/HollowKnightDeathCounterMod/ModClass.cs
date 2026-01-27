e387f07cd63bd96ad7d006cf220c0dfdb9cc1be1
JaessDev <jaess-dev@gmx.de>
2026-01-27 18:34:55 +0100

    add the death counter mod base

---
+++ HollowKnightStarterMod/HollowKnightDeathCounterMod/ModClass.cs
@@ -0,0 +1,40 @@
+﻿// using UObject = UnityEngine.Object;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using GlobalEnums;
+using HK.Domain;
+using HollowKnightDeathCounterMod.Domain.Model;
+using Modding;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace HollowKnightDeathCounterMod;
+
+public class HollowKnightDeathCounterMod : Mod
+{
+    private ICommunication _connector;
+    public HollowKnightDeathCounterMod() : base("HkDeathCounter")
+    {
+    }
+
+    public override string GetVersion() => "v0.0.1";
+
+
+    public override void Initialize()
+    {
+        On.HeroController.Die += (On.HeroController.orig_Die org, HeroController self) =>
+        {
+            var res = org(self);
+            OnDeath(self);
+            return res;
+        };
+    }
+}
---
+++ HollowKnightStarterMod/HollowKnightDeathCounterMod/ReadMe.md
@@ -0,0 +1,5 @@
+# HollowKnightDeathCounterMod
+
+A mod for the game Hollow Knight.
+
+https://radiance.synthagen.net/apidocs/search.html?q=geo&check_keywords=yes&area=default
